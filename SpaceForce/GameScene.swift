043b744c297c489637989e37832d1320ef7367a5
Johel <johelzarco@gmail.com>
2019-06-20 12:12:55 -0500

    First comm,  space game based on SpriteKit.

--- SpaceForce/GameScene.swift
+++ SpaceForce/GameScene.swift
@@ -1,110 +1,95 @@
 //
 //  GameScene.swift
-//  SpaceForce
+//  SpaceForce Demo
 //
 //  Created by JohelCzarco on 6/18/19.
 //  Copyright © 2019 JohelCzarco. All rights reserved.
 //
 
 import SpriteKit
-import GameplayKit
+
+@objcMembers
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    let player = SKSpriteNode(imageNamed: "player.png")
+    var touchingPlayer : Bool = false
+    var gameTimer : Timer?
+    var nodeNum : Int = 0
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView) {
+        // scene ready to run, background
+        let background = SKSpriteNode(imageNamed: "space.png")
+        background.zPosition = -1 // below other nodes
+        addChild(background)
+        // space dust stuff
+        if let particles = SKEmitterNode(fileNamed: "SpaceDust"){
+            particles.advanceSimulationTime(10)
+            particles.position.x = 512
+            addChild(particles)
+        }
+        // player stuff
+        player.position.x = -300
+        player.zPosition = 1
+        addChild(player)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self) // return loc inside scene
+        let tappedNodes = nodes(at: location)// nodes precisely in that loc
+        if tappedNodes.contains(player) {touchingPlayer = true}
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard touchingPlayer else {return}
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        player.position = location
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        touchingPlayer = false
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
+    
+    func createEnemy() {
+        
+        let astIndex : Int = Int.random(in: 0 ... 2)
+        let asteroid : SKSpriteNode
+        if astIndex == 0 {
+            asteroid = SKSpriteNode(imageNamed: "redAsteroid.png")
+            modEnemy(asteroid: asteroid)
+        } else if astIndex == 1 {
+             asteroid = SKSpriteNode(imageNamed: "greyAsteroid.png")
+            modEnemy(asteroid: asteroid)
+        } else if astIndex == 2 {
+             asteroid = SKSpriteNode(imageNamed: "blackAsteroid.png")
+            modEnemy(asteroid: asteroid)
+        } else {return}
+    }
+    
+    func modEnemy(asteroid : SKSpriteNode){
+        let angRotation : Double = Double.random(in: 0.5 ... 3.0)
+        print(angRotation)
+        asteroid.position = CGPoint(x: 320, y: Int.random(in: -260 ... 260))
+        asteroid.name = "enemy"
+        asteroid.zPosition = 1
+        addChild(asteroid)
+        asteroid.physicsBody = SKPhysicsBody(texture: asteroid.texture!, size: asteroid.size)
+        asteroid.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        asteroid.physicsBody?.linearDamping = 0
+        // asteroid rotation
+        let oneRevolution = SKAction.rotate(byAngle: CGFloat.pi * 2, duration: angRotation)
+        let repeatRotation = SKAction.repeatForever(oneRevolution)
+        asteroid.run(repeatRotation)
+    }
+    
 }
