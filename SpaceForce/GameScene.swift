449a2ecfeaaa15d2190b2ebc59f8361e7b11662f
Johel <johelzarco@gmail.com>
2019-06-20 19:03:56 -0500

    Added contact test and explosion effect.

--- SpaceForce/GameScene.swift
+++ SpaceForce/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 @objcMembers
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let player = SKSpriteNode(imageNamed: "player.png")
     var touchingPlayer : Bool = false
@@ -31,9 +31,14 @@ class GameScene: SKScene {
         // player stuff
         player.position.x = -300
         player.zPosition = 1
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.categoryBitMask = 1
+        player.physicsBody?.contactTestBitMask = 1
+        player.name = "spaceship"
         addChild(player)
         
         gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        physicsWorld.contactDelegate = self
     
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -61,35 +66,69 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
-        
+        // Enemy based on random image
         let astIndex : Int = Int.random(in: 0 ... 2)
         let asteroid : SKSpriteNode
+        
         if astIndex == 0 {
             asteroid = SKSpriteNode(imageNamed: "redAsteroid.png")
             modEnemy(asteroid: asteroid)
         } else if astIndex == 1 {
-             asteroid = SKSpriteNode(imageNamed: "greyAsteroid.png")
+            asteroid = SKSpriteNode(imageNamed: "greyAsteroid.png")
             modEnemy(asteroid: asteroid)
         } else if astIndex == 2 {
-             asteroid = SKSpriteNode(imageNamed: "blackAsteroid.png")
+            asteroid = SKSpriteNode(imageNamed: "blackAsteroid.png")
             modEnemy(asteroid: asteroid)
         } else {return}
     }
     
     func modEnemy(asteroid : SKSpriteNode){
-        let angRotation : Double = Double.random(in: 0.5 ... 3.0)
-        print(angRotation)
+        
+        let angRotation : Double = Double.random(in: 0.5 ... 2.5)// random rotation speed
+        //print(angRotation)
         asteroid.position = CGPoint(x: 320, y: Int.random(in: -260 ... 260))
         asteroid.name = "enemy"
         asteroid.zPosition = 1
+        asteroid.physicsBody?.contactTestBitMask = 1
+        asteroid.physicsBody?.categoryBitMask = 0 // avoid collision with itself
         addChild(asteroid)
         asteroid.physicsBody = SKPhysicsBody(texture: asteroid.texture!, size: asteroid.size)
-        asteroid.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        asteroid.physicsBody?.velocity = CGVector(dx: -400, dy: 0)
         asteroid.physicsBody?.linearDamping = 0
         // asteroid rotation
         let oneRevolution = SKAction.rotate(byAngle: CGFloat.pi * 2, duration: angRotation)
         let repeatRotation = SKAction.repeatForever(oneRevolution)
         asteroid.run(repeatRotation)
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.node?.name == "spaceship" || contact.bodyB.node?.name == "enemy" {
+            print("coliison")
+        }
+        
+        guard let nodeA = contact.bodyA.node else {return}
+        guard let nodeB = contact.bodyB.node else {return}
+        
+        if nodeA == player {
+            playerHit(nodeB)
+            print("player hitA")
+        } else {
+            playerHit(nodeA)
+            print("player hitB")
+        }
+    }
+    
+    func playerHit(_ node: SKNode){
+        print("player hit")
+//        let soundExplosion = SKAction.playSoundFileNamed("explosion.wav", waitForCompletion: false)
+//        run(soundExplosion)
+//        if let explosion = SKEmitterNode(fileNamed: "explosion.sks") {
+//            explosion.position = player.position
+//            explosion.zPosition = 3
+//            addChild(explosion)
+//        }
+        player.removeFromParent()
     }
     
 }
